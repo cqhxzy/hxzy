77ed30322b86c53d012d38942e7a79c6cd58da60
piazze <w.951027>
2019-01-23 15:53:02 +0800

    DepartmentDaoImpl

--- src/com/hxzy/dao/DepartmentDao.java
+++ src/com/hxzy/dao/DepartmentDao.java
@@ -2,5 +2,11 @@ package com.hxzy.dao;
 
 import com.hxzy.bean.Department;
 
+import java.util.List;
+
 public interface DepartmentDao extends CommonDao<Department>{
+    /**
+     * 全查
+     */
+    List<Department> queryAll();
 }
